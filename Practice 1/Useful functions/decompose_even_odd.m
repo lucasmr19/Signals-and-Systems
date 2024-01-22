0cb7724be558d748f6d6c04a63c0b32313aa9318
lucasmr19 <157109722+lucasmr19@users.noreply.github.com>
2024-01-22 11:27:59 +0100

    Update decompose_even_odd.m

--- Practice 1/Useful functions/decompose_even_odd.m
+++ Practice 1/Useful functions/decompose_even_odd.m
@@ -8,15 +8,6 @@ function [x_even, x_odd] = decompose_even_odd(x)
     %   x_even: Even component of the signal
     %   x_odd: Odd component of the signal
     %
-    % Description:
-    %   This function takes a signal 'x' and decomposes it into its even
-    %   (symmetric) and odd (antisymmetric) components using the formula:
-    %
-    %       x_even = (x + flip(x)) / 2;
-    %       x_odd = x - x_even;
-    %
-    %   where 'flip' reverses the order of the elements in the input signal.
-    %
     % Usage example:
     %
     %   % Define a signal
